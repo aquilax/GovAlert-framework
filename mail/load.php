3ad4297a8e782964dc0979ffe4a5f10c3a6fec4d
aquilax <evgeniy@wikia-inc.com>
2014-11-30 11:02:37 +0100

    Import paths

--- mail/load.php
+++ mail/load.php
@@ -1,7 +1,7 @@
 <?php
 $path = __DIR__;
 require_once($path . '/../streams/init.php');
-require_once(BASEPATH . '/streams/_classes/twitter.php');
+require_once(BASEPATH . '/_classes/twitter.php');
 require_once($path . '/strategy/tasks.php');
 
 strategy_processUrl('http://www.strategy.bg/PublicConsultations/View.aspx?lang=bg-BG&Id=1234');
--- streams/run.php
+++ streams/run.php
@@ -3,7 +3,7 @@ require_once('init.php');
 if ($debug) {
 	exit;
 }
-require_once("twitter.php");
+require_once(BASEPATH . "/_classes/twitter.php");
 
 echo "\n\nSTREAMS ".date("r")."\n";
 runTasks(count($argv)>1);
