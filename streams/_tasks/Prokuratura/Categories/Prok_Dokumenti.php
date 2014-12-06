22cf8ec8e817d03f32a2af0657aeeee5965eb469
aquilax <evgeniy@wikia-inc.com>
2014-12-06 08:21:51 +0100

    Prokuratura

---
+++ streams/_tasks/Prokuratura/Categories/Prok_Dokumenti.php
@@ -0,0 +1,38 @@
+<?php
+
+class Prok_Dokumenti extends Prokuratura {
+
+	protected $categoryId = 1;
+	protected $categoryName = 'документи';
+	protected $categoryURL = 'http://www.prb.bg/main/bg/Documents/';
+
+	function execute($html)
+	{
+		$items = $this->xpathDoc($html, "//div[@class='list-inner']");
+
+		$query = array();
+		foreach ($items as $item) {
+
+			$date = trim($item->childNodes->item(3)->textContent);
+			$date = mb_substr($date, 6, 4) . "-" . mb_substr($date, 3, 2) . "-" . mb_substr($date, 0, 2);
+			if (strtotime($date) < strtotime("-2 months"))
+				continue;
+			$description = trim($item->childNodes->item(5)->textContent);
+			$description = $this->cleanText($description);
+
+			$title = trim($item->childNodes->item(1)->textContent);
+			$title = $this->cleanTitle($title);
+			$title = "Документ: " . $this->cleanText($title);
+
+			$url = "http://www.prb.bg" . $item->childNodes->item(1)->firstChild->getAttribute("href");
+			$hash = md5($url);
+
+			$query[] = array($title, $description, $date, $url, $hash);
+		}
+
+		echo "Възможни " . count($query) . " нови документи\n";
+		$itemids = $this->saveItems($query);
+		$this->queueTweets($itemids);
+	}
+
+} 
---
+++ streams/_tasks/Prokuratura/Categories/Prok_Snimki.php
@@ -0,0 +1,53 @@
+<?php
+
+class Prok_Snimki extends Prokuratura {
+
+	protected $categoryId = 3;
+	protected $categoryName = 'галерии';
+	protected $categoryURL = 'http://www.prb.bg/main/bg/gallery/';
+
+	function execute($html)
+	{
+		$items = $this->xpathDoc($html, "//div[@class='list-inner']");
+
+		$query = array();
+		foreach ($items as $item) {
+			$date = trim($item->childNodes->item(5)->textContent);
+			$date = mb_substr($date, 6, 4) . "-" . mb_substr($date, 3, 2) . "-" . mb_substr($date, 0, 2);
+			if (strtotime($date) < strtotime("-2 weeks"))
+				continue;
+
+			$title = trim($item->childNodes->item(3)->textContent);
+			$title = $this->cleanTitle($title);
+			$title = "Снимки: " . $title;
+			$title = $this->cleanText($title);
+
+			$url = "http://www.prb.bg" . $item->childNodes->item(1)->getAttribute("href");
+			$hash = md5($url);
+			$media = array("image" => array());
+			$mhtml = $this->loadURL($url);
+			if (!$mhtml)
+				continue;
+
+			$mitems = $this->xpathDoc($mhtml, "//a[@class='thumb']");
+			foreach ($mitems as $mitem) {
+				$imageurl = $mitem->getAttribute("href");
+				$imageurl = "http://www.prb.bg$imageurl";
+				$imageurl = str_replace(array("logo", "pic"), "big", $imageurl);
+				$imageurl = $this->loadItemImage($imageurl, []);
+				if ($imageurl)
+					$media["image"][] = array($imageurl);
+			}
+
+			if (count($media["image"]) == 0)
+				$media = null;
+
+			$query[] = array($title, null, $date, $url, $hash, $media);
+		}
+
+		echo "Възможни " . count($query) . " нови галерии\n";
+		$itemids = $this->saveItems($query);
+		$this->queueTweets($itemids);
+	}
+
+} 
